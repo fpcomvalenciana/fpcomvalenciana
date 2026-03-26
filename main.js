fe6f30ba16a82a9884ec2a7bf12c75c23a421bbd
FP Comunitat Valenciana <fpcomvalenciana@gmail.com>
2026-03-26 12:29:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,263 @@
+// comarques.js — Comarques i municipis de la Comunitat Valenciana
+// V4 — 3 províncies
+
+export const provincies = {
+  'Alicante': { es: 'Alicante', val: 'Alacant' },
+  'Castellon': { es: 'Castellón', val: 'Castelló' },
+  'Valencia': { es: 'Valencia', val: 'València' },
+};
+
+export const comarcaMunicipis = {
+  'El Baix Maestrat': {
+    es: 'El Bajo Maestrazgo', val: 'El Baix Maestrat', provincia: 'Castellon',
+    municipis: {
+    'Alcalà de Xivert': { es:'Alcalà de Xivert', val:'Alcalà de Xivert' },
+    'Benicarló': { es:'Benicarló', val:'Benicarló' },
+    'Peníscola': { es:'Peníscola', val:'Peníscola' },
+    'Sant Mateu': { es:'Sant Mateu', val:'Sant Mateu' },
+    'Vinaròs': { es:'Vinaròs', val:'Vinaròs' }
+    }
+  },
+  'El Baix Vinalopó': {
+    es: 'El Bajo Vinalopó', val: 'El Baix Vinalopó', provincia: 'Alicante',
+    municipis: {
+    'Crevillent': { es:'Crevillent', val:'Crevillent' },
+    'Elx': { es:'Elx', val:'Elx' },
+    'Santa Pola': { es:'Santa Pola', val:'Santa Pola' }
+    }
+  },
+  'El Camp de Morvedre': {
+    es: 'El Camp de Morvedre', val: 'El Camp de Morvedre', provincia: 'Valencia',
+    municipis: {
+    'Sagunt': { es:'Sagunt', val:'Sagunt' }
+    }
+  },
+  'El Camp de Túria': {
+    es: 'El Camp de Túria', val: 'El Camp de Túria', provincia: 'Valencia',
+    municipis: {
+    'Benaguasil': { es:'Benaguasil', val:'Benaguasil' },
+    'Llíria': { es:'Llíria', val:'Llíria' }
+    }
+  },
+  'El Comtat': {
+    es: 'El Comtat', val: 'El Comtat', provincia: 'Alicante',
+    municipis: {
+    'Cocentaina': { es:'Cocentaina', val:'Cocentaina' },
+    'Muro de Alcoi': { es:'Muro de Alcoi', val:'Muro de Alcoi' }
+    }
+  },
+  'El Racó d\'Ademús': {
+    es: 'El Rincón de Ademuz', val: 'El Racó d\'Ademús', provincia: 'Valencia',
+    municipis: {
+    'Ademuz': { es:'Ademuz', val:'Ademuz' }
+    }
+  },
+  'El Vinalopó Mitjà': {
+    es: 'El Vinalopó Medio', val: 'El Vinalopó Mitjà', provincia: 'Alicante',
+    municipis: {
+    'Aspe': { es:'Aspe', val:'Aspe' },
+    'Elda': { es:'Elda', val:'Elda' },
+    'Monforte del Cid': { es:'Monforte del Cid', val:'Monforte del Cid' },
+    'Novelda': { es:'Novelda', val:'Novelda' },
+    'Petrer': { es:'Petrer', val:'Petrer' },
+    'el Pinós': { es:'el Pinós', val:'el Pinós' }
+    }
+  },
+  'Els Ports': {
+    es: 'Els Ports', val: 'Els Ports', provincia: 'Castellon',
+    municipis: {
+    'Morella': { es:'Morella', val:'Morella' },
+    'Vilafranca': { es:'Vilafranca', val:'Vilafranca' }
+    }
+  },
+  'L\'Alacantí': {
+    es: 'L\'Alacantí', val: 'L\'Alacantí', provincia: 'Alicante',
+    municipis: {
+    'Alicante': { es:'Alicante', val:'Alicante' },
+    'El Campello': { es:'El Campello', val:'El Campello' },
+    'Mutxamel': { es:'Mutxamel', val:'Mutxamel' },
+    'Sant Joan d\'Alacant': { es:'Sant Joan d\'Alacant', val:'Sant Joan d\'Alacant' },
+    'Sant Vicent del Raspeig': { es:'Sant Vicent del Raspeig', val:'Sant Vicent del Raspeig' },
+    'Xixona': { es:'Xixona', val:'Xixona' }
+    }
+  },
+  'L\'Alcalatén': {
+    es: 'L\'Alcalatén', val: 'L\'Alcalatén', provincia: 'Castellon',
+    municipis: {
+    'l\'Alcora': { es:'l\'Alcora', val:'l\'Alcora' }
+    }
+  },
+  'L\'Alcoià': {
+    es: 'L\'Alcoià', val: 'L\'Alcoià', provincia: 'Alicante',
+    municipis: {
+    'Alcoi': { es:'Alcoi', val:'Alcoi' }
+    }
+  },
+  'L\'Alt Maestrat': {
+    es: 'El Alto Maestrazgo', val: 'L\'Alt Maestrat', provincia: 'Castellon',
+    municipis: {
+    'Benassal': { es:'Benassal', val:'Benassal' }
+    }
+  },
+  'L\'Alt Millars': {
+    es: 'El Alto Mijares', val: 'L\'Alt Millars', provincia: 'Castellon',
+    municipis: {
+    'Montanejos': { es:'Montanejos', val:'Montanejos' }
+    }
+  },
+  'L\'Alt Palància': {
+    es: 'El Alto Palancia', val: 'L\'Alt Palància', provincia: 'Castellon',
+    municipis: {
+    'Jérica-Viver': { es:'Jérica-Viver', val:'Jérica-Viver' },
+    'Segorbe': { es:'Segorbe', val:'Segorbe' }
+    }
+  },
+  'L\'Alt Vinalopó': {
+    es: 'El Alto Vinalopó', val: 'L\'Alt Vinalopó', provincia: 'Alicante',
+    municipis: {
+    'Banyeres de Mariola': { es:'Banyeres de Mariola', val:'Banyeres de Mariola' },
+    'Biar': { es:'Biar', val:'Biar' },
+    'Ibi': { es:'Ibi', val:'Ibi' },
+    'Onil': { es:'Onil', val:'Onil' },
+    'Sax': { es:'Sax', val:'Sax' },
+    'Villena': { es:'Villena', val:'Villena' }
+    }
+  },
+  'L\'Horta': {
+    es: 'L\'Horta', val: 'L\'Horta', provincia: 'Valencia',
+    municipis: {
+    'Alaquàs': { es:'Alaquàs', val:'Alaquàs' },
+    'Alboraia': { es:'Alboraia', val:'Alboraia' },
+    'Aldaia': { es:'Aldaia', val:'Aldaia' },
+    'Alfafar': { es:'Alfafar', val:'Alfafar' },
+    'Burjassot': { es:'Burjassot', val:'Burjassot' },
+    'Catarroja': { es:'Catarroja', val:'Catarroja' },
+    'Godella': { es:'Godella', val:'Godella' },
+    'Manises': { es:'Manises', val:'Manises' },
+    'Mislata': { es:'Mislata', val:'Mislata' },
+    'Moncada': { es:'Moncada', val:'Moncada' },
+    'Paterna': { es:'Paterna', val:'Paterna' },
+    'Puçol': { es:'Puçol', val:'Puçol' },
+    'Quart de Poblet': { es:'Quart de Poblet', val:'Quart de Poblet' },
+    'Sedaví': { es:'Sedaví', val:'Sedaví' },
+    'Torrent': { es:'Torrent', val:'Torrent' },
+    'València': { es:'València', val:'València' }
+    }
+  },
+  'La Costera': {
+    es: 'La Costera', val: 'La Costera', provincia: 'Valencia',
+    municipis: {
+    'Xàtiva': { es:'Xàtiva', val:'Xàtiva' }
+    }
+  },
+  'La Marina Alta': {
+    es: 'La Marina Alta', val: 'La Marina Alta', provincia: 'Alicante',
+    municipis: {
+    'Benissa': { es:'Benissa', val:'Benissa' },
+    'Calp': { es:'Calp', val:'Calp' },
+    'Dénia': { es:'Dénia', val:'Dénia' },
+    'Gata de Gorgos': { es:'Gata de Gorgos', val:'Gata de Gorgos' },
+    'Ondara': { es:'Ondara', val:'Ondara' },
+    'Pedreguer': { es:'Pedreguer', val:'Pedreguer' },
+    'Teulada': { es:'Teulada', val:'Teulada' },
+    'Xàbia': { es:'Xàbia', val:'Xàbia' }
+    }
+  },
+  'La Marina Baixa': {
+    es: 'La Marina Baixa', val: 'La Marina Baixa', provincia: 'Alicante',
+    municipis: {
+    'Altea': { es:'Altea', val:'Altea' },
+    'Benidorm': { es:'Benidorm', val:'Benidorm' },
+    'Callosa d\'en Sarrià': { es:'Callosa d\'en Sarrià', val:'Callosa d\'en Sarrià' },
+    'l\'Alfàs del Pi': { es:'l\'Alfàs del Pi', val:'l\'Alfàs del Pi' },
+    'la Nucia': { es:'la Nucia', val:'la Nucia' },
+    'la Vila Joiosa': { es:'la Vila Joiosa', val:'la Vila Joiosa' }
+    }
+  },
+  'La Plana Alta': {
+    es: 'La Plana Alta', val: 'La Plana Alta', provincia: 'Castellon',
+    municipis: {
+    'Almassora': { es:'Almassora', val:'Almassora' },
+    'Benicàssim': { es:'Benicàssim', val:'Benicàssim' },
+    'Castelló de la Plana': { es:'Castelló de la Plana', val:'Castelló de la Plana' },
+    'Orpesa': { es:'Orpesa', val:'Orpesa' },
+    'Torreblanca': { es:'Torreblanca', val:'Torreblanca' },
+    'Vall d\'Alba': { es:'Vall d\'Alba', val:'Vall d\'Alba' }
+    }
+  },
+  'La Plana Baixa': {
+    es: 'La Plana Baja', val: 'La Plana Baixa', provincia: 'Castellon',
+    municipis: {
+    'Almenara': { es:'Almenara', val:'Almenara' },
+    'Betxí': { es:'Betxí', val:'Betxí' },
+    'Borriana': { es:'Borriana', val:'Borriana' },
+    'Nules': { es:'Nules', val:'Nules' },
+    'Onda': { es:'Onda', val:'Onda' },
+    'Vila-real': { es:'Vila-real', val:'Vila-real' },
+    'la Vall d\'Uixó': { es:'la Vall d\'Uixó', val:'la Vall d\'Uixó' }
+    }
+  },
+  'La Plana d\'Utiel-Requena': {
+    es: 'La Plana de Utiel-Requena', val: 'La Plana d\'Utiel-Requena', provincia: 'Valencia',
+    municipis: {
+    'Requena': { es:'Requena', val:'Requena' }
+    }
+  },
+  'La Ribera Alta': {
+    es: 'La Ribera Alta', val: 'La Ribera Alta', provincia: 'Valencia',
+    municipis: {
+    'Albalat de la Ribera': { es:'Albalat de la Ribera', val:'Albalat de la Ribera' },
+    'Alberic': { es:'Alberic', val:'Alberic' },
+    'Algemesí': { es:'Algemesí', val:'Algemesí' },
+    'Alzira': { es:'Alzira', val:'Alzira' },
+    'Llombai': { es:'Llombai', val:'Llombai' }
+    }
+  },
+  'La Ribera Baixa': {
+    es: 'La Ribera Baja', val: 'La Ribera Baixa', provincia: 'Valencia',
+    municipis: {
+    'Albal': { es:'Albal', val:'Albal' },
+    'Cullera': { es:'Cullera', val:'Cullera' }
+    }
+  },
+  'La Safor': {
+    es: 'La Safor', val: 'La Safor', provincia: 'Valencia',
+    municipis: {
+    'Beniarjó': { es:'Beniarjó', val:'Beniarjó' },
+    'Benirredrà': { es:'Benirredrà', val:'Benirredrà' },
+    'Gandia': { es:'Gandia', val:'Gandia' },
+    'Tavernes de la Valldigna': { es:'Tavernes de la Valldigna', val:'Tavernes de la Valldigna' }
+    }
+  },
+  'La Vall d\'Albaida': {
+    es: 'El Valle de Albaida', val: 'La Vall d\'Albaida', provincia: 'Valencia',
+    municipis: {
+    'Aielo de Malferit': { es:'Aielo de Malferit', val:'Aielo de Malferit' },
+    'Albaida': { es:'Albaida', val:'Albaida' },
+    'Ontinyent': { es:'Ontinyent', val:'Ontinyent' }
+    }
+  },
+  'La Vega Baixa': {
+    es: 'La Vega Baja', val: 'La Vega Baixa', provincia: 'Alicante',
+    municipis: {
+    'Albatera': { es:'Albatera', val:'Albatera' },
+    'Almoradí': { es:'Almoradí', val:'Almoradí' },
+    'Benejúzar': { es:'Benejúzar', val:'Benejúzar' },
+    'Bigastro': { es:'Bigastro', val:'Bigastro' },
+    'Callosa de Segura': { es:'Callosa de Segura', val:'Callosa de Segura' },
+    'Catral': { es:'Catral', val:'Catral' },
+    'Dolores': { es:'Dolores', val:'Dolores' },
+    'Guardamar del Segura': { es:'Guardamar del Segura', val:'Guardamar del Segura' },
+    'Jacarilla': { es:'Jacarilla', val:'Jacarilla' },
+    'Los Montesinos': { es:'Los Montesinos', val:'Los Montesinos' },
+    'Orihuela': { es:'Orihuela', val:'Orihuela' },
+    'Pilar de la Horadada': { es:'Pilar de la Horadada', val:'Pilar de la Horadada' },
+    'Rojales': { es:'Rojales', val:'Rojales' },
+    'Torrevieja': { es:'Torrevieja', val:'Torrevieja' }
+    }
+  },
+};
+
+export const comarques = Object.keys(comarcaMunicipis).sort();
+
+// Total: 27 comarques
